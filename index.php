f0c2c0feb0e245f81f8f2931f623f032a0ecf2f8
Joaquin Perez <joaquinperez@Joaquins-MacBook-Air.local>
2023-04-13 20:25:51 -0500

    Changed home page title

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ window.location = "myProfile.php";
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Home Page</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 <link href="https://fonts.googleapis.com/css?family=Abel|Quicksand" rel="stylesheet">
 </head>
